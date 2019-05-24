af0388aa4a8aae719f6efd9aec3e7963f130a874
Michael R.F <mrflowers22@uwalumni.com>
2019-05-24 14:06:56 -0400

    implemented the persistence functionality for workout data model

--- Anyplace Fitness/Anyplace Fitness/Trainer Models/Model Controllers/WorkoutController.swift
+++ Anyplace Fitness/Anyplace Fitness/Trainer Models/Model Controllers/WorkoutController.swift
@@ -10,18 +10,29 @@ import Foundation
 class WorkoutController {
     var workouts = [Workout]()
     
+    var persistenceURL: URL? {
+        let fileManger = FileManager.default
+        guard let documentDirectory = fileManger.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let fileName = documentDirectory.appendingPathComponent("workouts.plist")
+        return fileName
+    }
+    
+    init() {
+        loadFromPersistentStore()
+    }
+    
     func createWorkout(name: String, schedule: String, location: String, instructorName: String){
         let newWorkout = Workout(name: name, schedule: schedule, location: location, instructorName: instructorName)
         workouts.append(newWorkout)
         
-        //SAVE TO PERSISTENT STORE
+        saveToPersistentStore()
     }
     
     func delete(workout: Workout){
         guard let workoutToDelete = workouts.firstIndex(of: workout) else { return }
             workouts.remove(at: workoutToDelete)
         
-        //SAVE TO PERSISTENT STORE
+        saveToPersistentStore()
     }
     
     func update(workout: Workout, newName: String, newSchedule: String, newLocation: String, newClient: [Client]?, newPunchPass: [PunchPass]?, newImage: String?){
@@ -31,14 +42,14 @@ class WorkoutController {
         workout.clients = newClient
         workout.image = newImage
         
-        //SAVE TO PERSISTENT STORE
+       saveToPersistentStore()
     }
     
     //add client to workout
     func add(client: Client, toWorkout: Workout){
         toWorkout.clients?.append(client)
         
-        //SAVE TO PERSISTENT STORE
+        saveToPersistentStore()
     }
     
     //delete client from workout
@@ -46,14 +57,44 @@ class WorkoutController {
         guard let clientToRemove = workout.clients?.firstIndex(of: client) else { return }
         workout.clients?.remove(at: clientToRemove)
         
-        //SAVE TO PERSISTENT STORE
+        saveToPersistentStore()
     }
     
     //punchpass to workout
     func include(punchPasses: [PunchPass], toWorkout workout: Workout){
         workout.punchPass = punchPasses
         
-        //SAVE TO PERSISTENT STORE
+        saveToPersistentStore()
     }
 
+    func saveToPersistentStore(){
+        guard let url = persistenceURL else {
+            print("Error unwrapping workouts: url savingToPersistentStore")
+            return }
+        
+        let propertyListEncoder = PropertyListEncoder()
+        do {
+            let data = try propertyListEncoder.encode(workouts)
+            try data.write(to: url)
+        } catch  {
+            print("Error encoding workouts model to persistent store: \(error.localizedDescription)")
+            return
+        }
+    }
+    
+    func loadFromPersistentStore(){
+        //we have to check to see if url path is there by checking the filemanager's file Exists method
+        let fm = FileManager.default
+        guard let url = persistenceURL, fm.fileExists(atPath: url.path) else { return }
+        
+        let propertyListDecoder = PropertyListDecoder()
+        do {
+            let data = try Data(contentsOf: url)
+            let workouts = try propertyListDecoder.decode([Workout].self, from: data)
+            self.workouts = workouts
+        } catch  {
+            print("Error decoding workouts data in the loadFromPersitenceStore do-catch: \(error.localizedDescription)")
+            return
+        }
+    }
 }
