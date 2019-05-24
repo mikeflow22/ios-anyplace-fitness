dfdc7657489ecfc6bec5618acb6834be34d9f2d1
Michael R.F <mrflowers22@uwalumni.com>
2019-05-24 09:48:15 -0400

    blah blah blah

--- Anyplace Fitness/Anyplace Fitness/View Controllers/Table View controllers/WorkoutListTableViewController.swift
+++ Anyplace Fitness/Anyplace Fitness/View Controllers/Table View controllers/WorkoutListTableViewController.swift
@@ -27,16 +27,14 @@ class WorkoutListTableViewController: UITableViewController {
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorkoutCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
-
+  
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
