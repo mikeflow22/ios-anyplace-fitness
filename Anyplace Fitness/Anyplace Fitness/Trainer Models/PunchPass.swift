af6f5426e71fe79ec66c31cf558757997b92f073
Michael R.F <mrflowers22@uwalumni.com>
2019-05-24 12:04:48 -0400

    Implemented the PunchPass model logic

---
+++ Anyplace Fitness/Anyplace Fitness/Trainer Models/PunchPass.swift
@@ -0,0 +1,15 @@
+//
+//  PunchPass.swift
+//  Anyplace Fitness
+//
+//  Created by Michael Flowers on 5/24/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct PunchPass {
+    var day: Int
+    var price: Double
+    var isSet: Bool
+}
