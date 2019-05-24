f019170131f216eabc95a2332a5edeed83c91cf8
Michael R.F <mrflowers22@uwalumni.com>
2019-05-24 11:07:29 -0400

    update the table view cell stack view constraints

---
+++ Anyplace Fitness/Anyplace Fitness/View Controllers/Table View Cells/PunchPassCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PunchPassCellTableViewCell.swift
+//  Anyplace Fitness
+//
+//  Created by Michael Flowers on 5/24/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+//DELEGATE PROTOCOL TO CHECK/TOGGLE THE BUTTON IMAGE THAT DICTATES IF THIS IS AVAILABLE OR NOT
+
+class PunchPassCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
